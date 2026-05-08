89427de07e99be5989f1dff2f3d4b8d55d054956
Dimitri <dimitrimike26@gmail.com>
2026-05-08 08:36:16 -0300

    Atualização 8

--- exercicio_avaliacao/sistema_notas.py
+++ exercicio_avaliacao/sistema_notas.py
@@ -1,23 +1,24 @@
 print("Sistema de Gerenciamento de Notas de Alunos")
 
-# Boletim da Turma
+# Banco de dados com o boletim da Turma
 boletim = []
 
+# Laço principal que mantém o programa em execução
 while True: 
     print(f''' --- Menu Principal ---
           [1] - Cadastrar Aluno
           [2] - Acessar Boletim da Turma
           [3] - Encerrar ''')
     comando = input ('Escolha uma opção:')
+    # Lógica para cadastramento de alunos
     if comando == '1':
         nome_aluno = input('Digite o nome do aluno: ')
         nota_1 = float(input('Digite a primeira nota do aluno: '))
         nota_2 = float(input('Digite a segunda nota do aluno: '))
         nota_3 = float(input('Digite a terceira nota do aluno: '))
+        # Lógica para cálculo da média e condicionais
         media = (nota_1 + nota_2 + nota_3)/3
-        #if nota_1 < 0 or nota_1 > 10:
-            print("Nota inválida")
-        elif media >= 7:
+        if media >= 7:
             print(f'Média{media}: Aprovado')
             situacao = 'Aprovado'
         elif media >= 5 < 7:
@@ -26,12 +27,14 @@ while True:
         elif media < 5:
             print(f'Média{media}: Reprovado')
             situacao = 'Reprovado'
+        # Lógica para registro no banco de dados
         registro = f'''
         Aluno:{nome_aluno} 
         Notas:{nota_1},{nota_2},{nota_3}
         Média:{media}
         Situação:{situacao}'''
         boletim.append(registro)
+    # Lógica para vizualização do banco de dados
     elif comando == '2':
         print('-- Boletim da Turma --')
         if not boletim:
@@ -39,6 +42,7 @@ while True:
         else:
             for aluno in boletim:
                 print(aluno)
+    # Lógica para encerrar o programa
     elif comando == '3':
         print('Encerando o sistema')
         break
